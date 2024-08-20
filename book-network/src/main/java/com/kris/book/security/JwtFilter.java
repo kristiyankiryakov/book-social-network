fb45e8248efd89adc3b35040a56713d9f74b63ca
Kristiyan Kiryakov <126320553+kristiyankiryakov@users.noreply.github.com>
2024-08-20 10:00:16 +0300

    implement jwt service

--- book-network/src/main/java/com/kris/book/security/JwtFilter.java
+++ book-network/src/main/java/com/kris/book/security/JwtFilter.java
@@ -1,7 +1,41 @@
 package com.kris.book.security;
 
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.constraints.NotNull;
+import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Service;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
 
 @Service
-public class JwtFilter {
+public class JwtFilter extends OncePerRequestFilter {
+
+    private final JwtService JwtService;
+
+    @Override
+    protected void doFilterInternal(
+            @NotNull HttpServletRequest request,
+            @NotNull HttpServletResponse response,
+            @NotNull FilterChain filterChain
+    )
+            throws ServletException, IOException {
+        if (request.getServletPath().contains("api/v1/auth")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+        final String authHeader = request.getHeader(HttpHeaders.AUTHORIZATION);
+        final String jwt;
+        final String userEmail;
+        if(authHeader == null || !authHeader.startsWith("Bearer ")){
+            filterChain.doFilter(request, response);
+            return;
+        }
+        int START_OF_TOKEN = 7;
+        jwt = authHeader.substring(START_OF_TOKEN);
+        userEmail = jwtService.extractUsername(jwt);
+    }
 }
