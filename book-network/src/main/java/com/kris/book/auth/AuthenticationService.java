8a88cc05ffb1e3978f9a56bb745fb98e4bcc8c8f
Kristiyan Kiryakov <126320553+kristiyankiryakov@users.noreply.github.com>
2024-08-20 10:53:04 +0300

    implement register method

--- book-network/src/main/java/com/kris/book/auth/AuthenticationService.java
+++ book-network/src/main/java/com/kris/book/auth/AuthenticationService.java
@@ -1,10 +1,73 @@
 package com.kris.book.auth;
 
+import com.kris.book.role.RoleRepository;
+import com.kris.book.user.Token;
+import com.kris.book.user.TokenRepository;
+import com.kris.book.user.User;
+import com.kris.book.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
-@Service
-public class AuthenticationService {
-    public void register(RegistrationRequest request) {
+import java.security.SecureRandom;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
 
+@Service
+@RequiredArgsConstructor
+public class AuthenticationService {
+
+    private final RoleRepository roleRepository;
+    private final PasswordEncoder passwordEncoder;
+    private static final String USER_ROLE = "USER";
+    private static final int TOKEN_LENGTH = 6;
+    private final UserRepository userRepository;
+    private final TokenRepository tokenRepository;
+
+    public void register(RegistrationRequest request) {
+        var userRole = roleRepository.findByName(USER_ROLE)
+                .orElseThrow(() -> new IllegalStateException("ROLE " + USER_ROLE + "was not initialized"));
+        var user = User.builder()
+                .firstname(request.getFirstname())
+                .lastname(request.getLastname())
+                .email(request.getEmail())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .accountLocked(false)
+                .enabled(false)
+                .roles(List.of(userRole))
+                .build();
+        userRepository.save(user);
+        sendValidationEmail(user);
+    }
+
+    private void sendValidationEmail(User user) {
+        var newToken = generateAndSaveActivationToken(user);
+
+    }
+
+    private String generateAndSaveActivationToken(User user) {
+
+        String generatedToken = generateActivationCode();
+        var token = Token.builder()
+                .token(generatedToken)
+                .createdAt(LocalDateTime.now())
+                .expiresAt(LocalDateTime.now().plusMinutes(15))
+                .user(user)
+                .build();
+        tokenRepository.save(token);
+        return generatedToken;
+    }
+
+    private String generateActivationCode() {
+        String characters = "0123456789";
+        StringBuilder codeBuilder = new StringBuilder();
+        SecureRandom secureRandom = new SecureRandom();
+        for (int i = 0; i < AuthenticationService.TOKEN_LENGTH; i++) {
+            int randomIndex = secureRandom.nextInt(characters.length());
+            codeBuilder.append(characters.charAt(randomIndex));
+        }
+
+        return codeBuilder.toString();
     }
 }
